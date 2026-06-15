ec358896e23372a5412b2f9b67ef0604a7841395
khasanov-ibroxim <xibroxim11@gmail.com>
2026-06-15 16:27:19 +0500

    sad

--- lib/api/client.ts
+++ lib/api/client.ts
@@ -18,7 +18,7 @@ import {
   PaymentUrlResponse
 } from './types';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://textile.okach-admin.uz/api';
 
 class ApiClient {
   private baseUrl: string;
